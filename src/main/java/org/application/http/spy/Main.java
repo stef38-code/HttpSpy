0c2a57f4392bbae71fb15a310bce00cb24a2ef29
stephane <stephane.betton@gmail.com>
2024-12-15 18:32:21 +0100

    exemple 2

--- src/main/java/org/application/http/spy/Main.java
+++ src/main/java/org/application/http/spy/Main.java
@@ -1,63 +1,61 @@
 package org.application.http.spy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.mock.Expectation;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpForward;
 import org.mockserver.model.HttpResponse;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.slf4j.LoggerFactory.getLogger;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        try (MockServerClient mockServerClient = new MockServerClient("127.0.0.1", 8081)) {
+    private static final Logger log = getLogger(Main.class);
 
-            // Configuration pour écouter sur le port 8080
-            HttpForward httpForward = HttpForward.forward().withScheme(HttpForward.Scheme.HTTPS).withHost("swapi.dev").withPort(443);
-            HttpResponse reponse = HttpResponse.response();
-            mockServerClient.when(
-                            HttpRequest.request()
-                                    .withPath("/api/people/.*")
-                                    .withMethod("GET")
-                    ).respond(reponse);//.forward(httpForward);
-            mockServerClient.openUI();
-//                    .respond(
-//                            (ExpectationResponseCallback) httpForward
-//                    )
-//                    .withCallback(
-//                            (HttpRequest request) -> {
-//                                ObjectMapper mapper = new ObjectMapper();
-//                                try {
-//                                    // Enregistrer la requête
-//                                    mapper.writeValue(new File("request.json"), request);
-//
-//                                    // Enregistrer la réponse (sera complétée après l'appel à swapi.dev)
-//                                    // ...
-//                                } catch (IOException e) {
-//                                    e.printStackTrace();
-//                                }
-//                                return HttpResponse.response();
-//                            }
-//                    );
-        }
+    public static void main(String[] args) throws IOException {
+        clientAndServerLoad();
+
     }
+    private static void clientAndServerLoad() throws IOException {
+        log.info("start clientAndServer");
+        ClientAndServer clientAndServer = new ClientAndServer(8081);
+
+        // Charger le fichier init.json
+        ObjectMapper objectMapper = new ObjectMapper();
+        URL resource = Main.class.getClassLoader().getResource("init.json");
+        assert resource != null;
+        Expectation[] expectations = objectMapper.readValue(new File(resource.getFile()), Expectation[].class);
+
+        // Envoyer les attentes à MockServer
+        clientAndServer.upsert(expectations);
+
+    }
+
+    private static void clientAndServer() {
+        log.info("start clientAndServer");
+        HttpForward httpForward = HttpForward.forward().withScheme(HttpForward.Scheme.HTTPS).withHost("dog.ceo").withPort(443);
+        ClientAndServer clientAndServer = new ClientAndServer(8081);
+
+        log.info("when");
+        clientAndServer
+                .when(
+                        request().withPath("/api/breeds/image/.*")
+                                .withMethod("GET")
+                )
+                .forward(
+                        httpForward
+                );
+
+    }
+
 }
-//public static class TestExpectationResponseCallback implements ExpectationResponseCallback {
-//
-//    @Override
-//    public HttpResponse handle(HttpRequest httpRequest) {
-//        if (httpRequest.getPath().getValue().endsWith("/path")) {
-//            return response()
-//                    .withStatusCode(HttpStatusCode.ACCEPTED_202.code())
-//                    .withHeaders(
-//                            header("x-callback", "test_callback_header"),
-//                            header("Content-Length", "a_callback_response".getBytes(UTF_8).length),
-//                            header("Connection", "keep-alive")
-//                    )
-//                    .withBody("a_callback_response");
-//        } else {
-//            return notFoundResponse();
-//        }
-//    }
-//}
-//
-//
